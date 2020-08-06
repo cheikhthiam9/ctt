b4ce32b11bb995ea37f3295502d7b5bbc6d9283b
Cheikh <cheikh.thiam9@gmail.com>
2020-08-06 11:10:11 -0500

    update resume

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,9 @@
 
     var slideIndex = 0;
     carousel();
+    check();
+    last();
+
 
     function carousel() {
         var i;
@@ -22,7 +25,42 @@
         x[slideIndex - 1].style.display = "block";
 
         setTimeout(carousel, 2000); // Change image every 2 seconds
+
     }
+
+    function check() {
+        var i;
+        var x = document.getElementsByClassName("NotifSlides");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > x.length) {
+            slideIndex = 1
+        }
+        x[slideIndex - 1].style.display = "block";
+
+        setTimeout(check, 2000); // Change image every 2 seconds
+
+    }
+
+    function last() {
+        var i;
+        var x = document.getElementsByClassName("PickSlides");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > x.length) {
+            slideIndex = 1
+        }
+        x[slideIndex - 1].style.display = "block";
+
+        setTimeout(last, 2000); // Change image every 2 seconds
+
+    }
+
+
 
     var $window = $(window),
         $body = $('body'),
